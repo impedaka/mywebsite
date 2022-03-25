aa611c9144df7e4ad7a3a2624fe502d31af0a0d8
impedaka <qinfengyu123@gmail.com>
2022-03-24 20:43:06 -0400

    work page bug fixed

--- pages/works.js
+++ pages/works.js
@@ -5,13 +5,7 @@ import { WorkGridItem } from "../components/grid-item";
 
 import sushiPreview from "../public/images/works/sushiPreview.png";
 import tinderPreview from "../public/images/works/tinderPreview.png";
-import thumbFourPainters from "../public/images/works/the-four-painters_eyecatch.jpg";
-import thumbMenkiki from "../public/images/works/menkiki_eyecatch.png";
-import thumbModeTokyo from "../public/images/works/modetokyo_eyecatch.png";
-import thumbStyly from "../public/images/works/styly_eyecatch.png";
-import thumbPichu2 from "../public/images/works/pichu2_eyecatch.png";
-import thumbFreeDBTagger from "../public/images/works/freedbtagger_eyecatch.png";
-import thumbAmembo from "../public/images/works/amembo_eyecatch.png";
+
 
 const Works = () => (
   <Layout title="Works">
